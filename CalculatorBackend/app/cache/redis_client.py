204a689b6dec41fa654a28d239463926b5278cdb
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-03-04 17:28:39 +0100

    fix: solve rate limiter

--- CalculatorBackend/app/cache/redis_client.py
+++ CalculatorBackend/app/cache/redis_client.py
@@ -1,8 +1,14 @@
+import os
 import redis
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class RedisClient:
     def __init__(self):
-        self.client = redis.Redis(host='localhost', port=6379, db=0)
+        # self.client = redis.Redis(host='localhost', port=6379, db=0)
+        redis_url = os.getenv("REDIS_URL")
+        self.client = redis.Redis.from_url(redis_url)
 
     def set_value(self, key, value, expiry):
         self.client.setex(key, expiry, value)
