20967a270f9ca8f79ac392dc1ddad6f3e45543b2
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-03-04 14:26:41 +0100

    feat: setup Flask server

--- CalculatorBackend/app/routes.py
+++ CalculatorBackend/app/routes.py
@@ -1,1 +1,11 @@
-print("dummy text")
+from . import app
+
+# Define the home route
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+# Define the test route
+@app.route("/test")
+def test():
+    return "I'm just testing..."
