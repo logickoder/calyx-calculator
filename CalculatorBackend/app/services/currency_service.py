0f60c6df590c1bb27bd4a0e1c8d26ab190ebfedd
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-03-04 18:33:39 +0100

    test: unathorized app ID

--- CalculatorBackend/app/services/currency_service.py
+++ CalculatorBackend/app/services/currency_service.py
@@ -2,7 +2,8 @@ import os
 import requests
 import datetime
 from dotenv import load_dotenv
-from app.utils.errors import NotFoundError
+from requests.exceptions import HTTPError
+from app.utils.errors import InvalidCurrencyCode, UnprocessableEntityError
 
 load_dotenv()
 
@@ -16,28 +17,51 @@ class CurrencyService:
         valid_codes = ["USD", "EUR", "GBP", "AED", "COL", "AUD"]
 
         if code not in valid_codes:
-            raise NotFoundError(f"Invalid currency code: {code}")
+            raise InvalidCurrencyCode(f"Invalid currency code: {code}")
 
     def get_conversion_rate(self, from_currency, to_currency):
         self.validate_currency_code(from_currency)
         self.validate_currency_code(to_currency)
 
+        print(from_currency)
+        print(to_currency)
+
         cache_key = f"{from_currency}_{to_currency}"
-        cached = self.redis_client.get_value(cache_key).decode('utf-8') if self.redis_client.get_value(cache_key) else None
         # cached = self.redis_client.get_value(cache_key)
+        cached = self.redis_client.get_value(cache_key).decode('utf-8') if self.redis_client.get_value(cache_key) else None
 
         if cached:
             rate, timestamp = cached.split('|')
             return {'rate': float(rate), 'timestamp': timestamp}
 
-        # Fetch from Open Exchange Rates if not cached or cache is outdated
-        url = f'https://openexchangerates.org/api/convert/1/{from_currency}/{to_currency}?app_id={self.app_id}'
-        response = requests.get(url).json()
-        rate = response['meta']['rate']
+        try:
+            url = f"https://openexchangerates.org/api/convert/1/{from_currency}/{to_currency}?app_id={self.app_id}&prettyprint=false"
+            headers = {"accept": "application/json"}
+            response = requests.get(url, headers=headers)
+            response.raise_for_status() 
 
-        # Generate timestamp at the moment of caching
-        timestamp = datetime.datetime.utcnow().isoformat() + "Z"
+            print("response")
+            print(response)
 
-        # Cache the new rate with timestamp
-        self.redis_client.set_value(cache_key, f"{rate}|{timestamp}", 3600)  # Cache for 1 hour
-        return {'rate': rate, 'timestamp': timestamp}
+            print("response text")
+            print(response.text)
+            
+            rate = response['meta']['rate']['etst']
+            if rate is None :
+                raise ValueError("Missing rate in the response")
+            
+            # Generate timestamp at the moment of caching
+            timestamp = datetime.datetime.utcnow().isoformat() + "Z"
+
+            # Cache the new rate with timestamp
+            self.redis_client.set_value(cache_key, f"{rate}|{timestamp}", 3600)  # Cache for 1 hour
+            return {'rate': rate, 'timestamp': timestamp}
+        
+        except HTTPError as http_err:
+            # HTTP error occurred
+            print(f"HTTP error occurred: {http_err}")  # Python 3.6+
+            raise UnprocessableEntityError(message="Failed to fetch conversion rate due to an external API error.")
+        except Exception as err:
+            # Other errors
+            print(f"An error occurred: {err}")
+            raise UnprocessableEntityError(message="An unexpected error occurred while fetching conversion rate.")
