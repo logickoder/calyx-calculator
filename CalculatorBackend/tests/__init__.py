c10d03b707c25f43db67042d71967f24f287874a
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-03-04 20:28:40 +0100

    feat: add unit tests

--- CalculatorBackend/tests/__init__.py
+++ CalculatorBackend/tests/__init__.py
@@ -1,1 +1,7 @@
-print("dummy text")
+"""
+This __init__.py file ensures the tests directory is treated as a Python package.
+
+This setup allows for easy discovery of test modules and makes it possible to run all tests using a test runner like unittest or pytest from the project root directory.
+
+While currently not containing any test setup or configurations, this file can be used in the future to define fixtures, test configurations, or common utilities for use across multiple test modules within this package.
+"""
